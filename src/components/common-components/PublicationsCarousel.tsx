5f3a1cb393753b2a670b0b9de61c56e2ee2a2ff6
triszt4n <trisztanpiller@gmail.com>
2021-07-29 21:42:13 +0200

    Indexpage carousels

--- src/components/common-components/PublicationsCarousel.tsx
+++ src/components/common-components/PublicationsCarousel.tsx
@@ -1,5 +1,6 @@
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import React from 'react'
+import Slider from 'react-slick'
 import PublicationProps from '../../utils/props/publication.props'
 
 export interface PublicationsCarouselProps {
@@ -7,32 +8,54 @@ export interface PublicationsCarouselProps {
 }
 
 const PublicationsCarousel: React.FC<PublicationsCarouselProps> = ({ nodes }) => (
-  <div className="owl-slide-3 owl-carousel">
+  <Slider
+    infinite={false}
+    slidesToShow={3}
+    slidesToScroll={1}
+    className="py-3"
+    responsive={[
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 512,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]}
+  >
     {nodes.map((publication) => {
       const featuredImage = getImage(publication.featuredImage)
       return (
-        <div key={publication.title} className="education-1-item">
-          <figure className="thumbnail">
-            {featuredImage && <GatsbyImage image={featuredImage} alt={publication.title} className="img-fluid" />}
-            <div className="category bg-secondary">
-              <h3>
-                {publication.journalTitle} {publication.year}
-              </h3>
+        <div key={publication.fields.slug} className="p-2">
+          <div className="education-1-item">
+            <figure className="thumbnail">
+              {featuredImage && <GatsbyImage image={featuredImage} alt={publication.frontmatter.title} className="img-fluid" />}
+              <div className="category bg-secondary">
+                <h3>
+                  {publication.frontmatter.journalTitle} {publication.frontmatter.year}
+                </h3>
+              </div>
+            </figure>
+            <div className="education-1-content pb-4">
+              <h2>{publication.frontmatter.title}</h2>
+              {/* eslint-disable-next-line react/no-danger */}
+              <div className="desc mb-4" dangerouslySetInnerHTML={{ __html: publication.html }} />
+              <p>
+                <a target="_blank" rel="noopener noreferrer" href={publication.frontmatter.url} className="btn btn-primary rounded-0 px-4">
+                  Bővebben
+                </a>
+              </p>
             </div>
-          </figure>
-          <div className="education-1-content pb-4">
-            <h2>{publication.title}</h2>
-            <p>{publication.description}</p>
-            <p>
-              <a target="_blank" rel="noopener noreferrer" href={publication.url} className="btn btn-primary rounded-0 px-4">
-                Bővebben
-              </a>
-            </p>
           </div>
         </div>
       )
     })}
-  </div>
+  </Slider>
 )
 
 export default PublicationsCarousel
