49abcbfbae3940497f4501348950ead726a8776d
triszt4n <trisztanpiller@gmail.com>
2021-07-30 02:17:43 +0200

    Fix import issue

--- src/components/carousels/index.tsx
+++ src/components/carousels/index.tsx
@@ -2,5 +2,6 @@ import EventsCarousel from './EventsCarousel'
 import ProjectsCarousel from './ProjectsCarousel'
 import PublicationsCarousel from './PublicationsCarousel'
 import SpecializationsCarousel from './SpecializationsCarousel'
+import StudentAchievementsCarousel from './StudentAchievementsCarousel'
 
-export { ProjectsCarousel, EventsCarousel, SpecializationsCarousel, PublicationsCarousel }
+export { ProjectsCarousel, EventsCarousel, SpecializationsCarousel, PublicationsCarousel, StudentAchievementsCarousel }
