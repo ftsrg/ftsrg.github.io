6468e25fb2abbfbec960b950e97c80c898fea944
triszt4n <trisztanpiller@gmail.com>
2021-08-01 00:46:05 +0200

    Fix after rebase

--- src/components/carousels/PublicationsCarousel.tsx
+++ src/components/carousels/PublicationsCarousel.tsx
@@ -31,22 +31,21 @@ const PublicationsCarousel: React.FC<PublicationsCarouselProps> = ({ nodes }) =>
     {nodes.map((publication) => {
       const featuredImage = getImage(publication.featuredImage)
       return (
-        <div key={publication.fields.slug} className="p-2">
+        <div key={publication.title} className="p-2">
           <div className="education-1-item">
             <figure className="thumbnail">
-              {featuredImage && <GatsbyImage image={featuredImage} alt={publication.frontmatter.title} className="img-fluid" />}
+              {featuredImage && <GatsbyImage image={featuredImage} alt={publication.title} className="img-fluid" />}
               <div className="category bg-secondary">
                 <h3>
-                  {publication.frontmatter.journalTitle} {publication.frontmatter.year}
+                  {publication.journalTitle} {publication.year}
                 </h3>
               </div>
             </figure>
             <div className="education-1-content pb-4">
-              <h2>{publication.frontmatter.title}</h2>
-              {/* eslint-disable-next-line react/no-danger */}
-              <div className="desc mb-4" dangerouslySetInnerHTML={{ __html: publication.html }} />
+              <h2>{publication.title}</h2>
+              <p>{publication.description}</p>
               <p>
-                <a target="_blank" rel="noopener noreferrer" href={publication.frontmatter.url} className="btn btn-primary rounded-0 px-4">
+                <a target="_blank" rel="noopener noreferrer" href={publication.url} className="btn btn-primary rounded-0 px-4">
                   Bővebben
                 </a>
               </p>
