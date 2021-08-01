7270ef1c57e482bc72903540b988e4d0dfdabca5
triszt4n <trisztanpiller@gmail.com>
2021-08-01 17:05:23 +0200

    Refactor footer to include navbar items

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,16 +4,12 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Button, Col, Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { FaFacebookF, FaGithub, FaTwitter } from 'react-icons/fa'
-import LanguageToggle from './LanguageToggle'
-
-interface INavbarItem {
-  title: string
-  href: string
-  children?: Array<{ title: string; href: string }>
-}
+import LanguageToggle from '../LanguageToggle'
+import NAVBAR_ITEMS from './navbar-items'
 
 const NavBar: React.FC = () => {
   const location = useLocation()
+  const { t } = useTranslation()
   const socialStyle = { width: '2.5rem', height: '2.5rem' }
 
   const [isShrinked, setShrinked] = useState(false)
@@ -29,57 +25,6 @@ const NavBar: React.FC = () => {
       window.removeEventListener('scroll', onScroll)
     }
   })
-
-  const { t } = useTranslation()
-  const NAVBAR_ITEMS: Array<INavbarItem> = [
-    {
-      title: t('nav.home.title'),
-      href: '/',
-      children: [
-        { title: t('nav.home.competences'), href: '#competences' },
-        { title: t('nav.home.education'), href: '#education' },
-        { title: t('nav.home.about'), href: '#about' },
-        { title: t('nav.home.projects'), href: '#projects' },
-        { title: t('nav.home.partners'), href: '#partners' },
-        { title: t('nav.home.news'), href: '#news' }
-      ]
-    },
-    {
-      title: t('nav.research.title'),
-      href: '/research/',
-      children: [
-        { title: t('nav.research.projects'), href: '#projects' },
-        { title: t('nav.research.publications'), href: '#publications' },
-        { title: t('nav.research.tools'), href: '#tools' },
-        { title: t('nav.research.events'), href: '#events' }
-      ]
-    },
-    {
-      title: t('nav.education.title'),
-      href: '/education/',
-      children: [
-        { title: t('nav.education.specializations'), href: '#specializations' },
-        { title: t('nav.education.courses'), href: '#courses' },
-        { title: t('nav.education.studentwork'), href: '#student-work' },
-        { title: t('nav.education.talentcare'), href: '#talentcare' },
-        { title: t('nav.education.achievements'), href: '#achievements' }
-      ]
-    },
-    {
-      title: t('nav.about.title'),
-      href: '/about/',
-      children: [
-        { title: t('nav.about.about'), href: '#about' },
-        { title: t('nav.about.members'), href: '#members' },
-        { title: t('nav.about.formermembers'), href: '#formermembers' },
-        { title: t('nav.about.students'), href: '#students' }
-      ]
-    },
-    {
-      title: t('nav.contact.title'),
-      href: '/contact/'
-    }
-  ]
 
   return (
     <>
@@ -117,10 +62,10 @@ const NavBar: React.FC = () => {
                 if (location.pathname === item.href) {
                   if (item.children) {
                     return (
-                      <NavDropdown key={item.href} title={item.title} id="basic-nav-dropdown" className="active" show>
+                      <NavDropdown key={item.id} title={t(item.title)} id="basic-nav-dropdown" className="active" show>
                         {item.children?.map((child) => (
                           <NavDropdown.Item as={AnchorLink} offset="70" key={child.href} href={child.href}>
-                            {child.title}
+                            {t(child.title)}
                           </NavDropdown.Item>
                         ))}
                       </NavDropdown>
@@ -128,15 +73,15 @@ const NavBar: React.FC = () => {
                   }
 
                   return (
-                    <Nav.Link className="active" key={item.href} href={item.href}>
-                      {item.title}
+                    <Nav.Link className="active" key={item.id} href={item.href}>
+                      {t(item.title)}
                     </Nav.Link>
                   )
                 }
 
                 return (
-                  <Nav.Link key={item.href} href={item.href}>
-                    {item.title}
+                  <Nav.Link key={item.id} href={item.href}>
+                    {t(item.title)}
                   </Nav.Link>
                 )
               })}
