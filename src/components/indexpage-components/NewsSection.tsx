d2d181bc3886e9f55d675a73ac85962037b88a77
triszt4n <trisztanpiller@gmail.com>
2021-08-10 15:29:18 +0200

    Change to defer

--- src/components/indexpage-components/NewsSection.tsx
+++ src/components/indexpage-components/NewsSection.tsx
@@ -5,8 +5,8 @@ import { Helmet } from 'react-helmet'
 const NewsSection: React.FC = () => (
   <div id="news" className="news-updates">
     <Helmet>
-      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v7.0" nonce="SRkXDokT" />
-      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
+      <script defer crossOrigin="anonymous" src="https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v7.0" nonce="SRkXDokT" />
+      <script defer crossOrigin="anonymous" src="https://platform.twitter.com/widgets.js" />
     </Helmet>
     <Container>
       <h2 className="section-title-underline text-center mb-5">
@@ -15,13 +15,7 @@ const NewsSection: React.FC = () => (
 
       <Row className="justify-content-center text-center mb-5">
         <Col lg={6} className="social-media-container mb-5 mb-lg-0">
-          <a
-            className="twitter-timeline"
-            data-adapt-container-width="true"
-            data-height="500"
-            data-width="400"
-            href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw"
-          >
+          <a className="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw">
             Tweets by ftsrg_bme
           </a>
         </Col>
