1d06a8479f9700c04f036c8dcbb34fc0ae6b521b
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2021-08-11 15:49:42 +0200

    Add social plugins (#170)
    * Add social plugins
    
    * Change to defer

--- src/components/indexpage-components/NewsSection.tsx
+++ src/components/indexpage-components/NewsSection.tsx
@@ -1,43 +1,38 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Helmet } from 'react-helmet'
 
 const NewsSection: React.FC = () => (
   <div id="news" className="news-updates">
+    <Helmet>
+      <script defer crossOrigin="anonymous" src="https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v7.0" nonce="SRkXDokT" />
+      <script defer crossOrigin="anonymous" src="https://platform.twitter.com/widgets.js" />
+    </Helmet>
     <Container>
       <h2 className="section-title-underline text-center mb-5">
         <span>Hírek és események</span>
       </h2>
 
       <Row className="justify-content-center text-center mb-5">
-        <Col lg={6} className="social-media-container">
-          <h2>ftsrg a Twitteren</h2>
-          <hr />
-          <a
-            className="twitter-timeline"
-            data-adapt-container-width="true"
-            data-height="500"
-            data-width="500"
-            href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw"
-          >
+        <Col lg={6} className="social-media-container mb-5 mb-lg-0">
+          <a className="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw">
             Tweets by ftsrg_bme
           </a>
         </Col>
         <Col lg={6} className="social-media-container">
-          <h2>ftsrg a Facebookon</h2>
-          <hr />
           <div
             className="fb-page"
             data-href="https://www.facebook.com/ftsrg/"
             data-tabs="timeline"
-            data-width="500"
-            data-height="600"
+            data-width="400"
+            data-height="500"
             data-small-header="false"
             data-adapt-container-width="true"
             data-hide-cover="false"
             data-show-facepile="true"
           >
             <blockquote cite="https://www.facebook.com/ftsrg/" className="fb-xfbml-parse-ignore">
-              <a href="https://www.facebook.com/ftsrg/">Hibatűrő Rendszerek Kutatócsoport</a>
+              <a href="https://www.facebook.com/ftsrg/">Kritikus Rendszerek Kutatócsoport</a>
             </blockquote>
           </div>
         </Col>
