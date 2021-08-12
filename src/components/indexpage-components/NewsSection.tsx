54a850ce5c75d8a9f3723acb06575951d332bf14
triszt4n <trisztanpiller@gmail.com>
2021-08-12 09:59:37 +0200

    Fix NewsSection after rebase

--- src/components/indexpage-components/NewsSection.tsx
+++ src/components/indexpage-components/NewsSection.tsx
@@ -1,76 +1,42 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import { Helmet } from 'react-helmet'
+import { useTranslation } from 'react-i18next'
 
-const NewsSection: React.FC = () => (
-  <div id="news" className="news-updates">
-    <Helmet>
-      <script defer crossOrigin="anonymous" src="https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v7.0" nonce="SRkXDokT" />
-      <script defer crossOrigin="anonymous" src="https://platform.twitter.com/widgets.js" />
-    </Helmet>
-    <Container>
-      <h2 className="section-title-underline text-center mb-5">
-        <span>Hírek és események</span>
-      </h2>
+const NewsSection: React.FC = () => {
+  const { t } = useTranslation()
 
-      <Row className="justify-content-center text-center mb-5">
-        <Col lg={6} className="social-media-container mb-5 mb-lg-0">
-          <a className="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw">
-            Tweets by ftsrg_bme
-          </a>
-        </Col>
-        <Col lg={6} className="social-media-container">
-          <div
-            className="fb-page"
-            data-href="https://www.facebook.com/ftsrg/"
-            data-tabs="timeline"
-            data-width="400"
-            data-height="500"
-            data-small-header="false"
-            data-adapt-container-width="true"
-            data-hide-cover="false"
-            data-show-facepile="true"
-          >
-            <blockquote cite="https://www.facebook.com/ftsrg/" className="fb-xfbml-parse-ignore">
-              <a href="https://www.facebook.com/ftsrg/">Kritikus Rendszerek Kutatócsoport</a>
-            </blockquote>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-)
+  return (
+    <div id="news" className="news-updates">
+      <Helmet>
+        <script defer crossOrigin="anonymous" src="https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v7.0" nonce="SRkXDokT" />
+        <script defer crossOrigin="anonymous" src="https://platform.twitter.com/widgets.js" />
+      </Helmet>
+      <Container>
+        <h2 className="section-title-underline text-center mb-5">
+          <span>{t('home.news.title')}</span>
+        </h2>
 
         <Row className="justify-content-center text-center mb-5">
-          <Col lg={6} className="social-media-container">
-            <h2>{t('home.news.onTwitter')}</h2>
-            <hr />
-            <a
-              className="twitter-timeline"
-              data-adapt-container-width="true"
-              data-height="500"
-              data-width="500"
-              href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw"
-            >
-              Tweets by ftsrg_bme
+          <Col lg={6} className="social-media-container mb-5 mb-lg-0">
+            <a className="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw">
+              {t('pages.home.heroTitle')} on Twitter
             </a>
           </Col>
           <Col lg={6} className="social-media-container">
-            <h2>{t('home.news.onFacebook')}</h2>
-            <hr />
             <div
               className="fb-page"
               data-href="https://www.facebook.com/ftsrg/"
               data-tabs="timeline"
-              data-width="500"
-              data-height="600"
+              data-width="400"
+              data-height="500"
               data-small-header="false"
               data-adapt-container-width="true"
               data-hide-cover="false"
               data-show-facepile="true"
             >
               <blockquote cite="https://www.facebook.com/ftsrg/" className="fb-xfbml-parse-ignore">
-                <a href="https://www.facebook.com/ftsrg/">Hibatűrő Rendszerek Kutatócsoport</a>
+                <a href="https://www.facebook.com/ftsrg/">{t('pages.home.heroTitle')} on Facebook</a>
               </blockquote>
             </div>
           </Col>
