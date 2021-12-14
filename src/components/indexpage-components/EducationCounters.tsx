b3219efee97160053fc38b5cae8a8e9c5548b87b
Zoltan Micskei <micskeiz@mit.bme.hu>
2021-12-14 09:39:37 +0100

    Update education counters on front page

--- src/components/indexpage-components/EducationCounters.tsx
+++ src/components/indexpage-components/EducationCounters.tsx
@@ -10,19 +10,23 @@ const counterData = [
     title: 'home.counters.c1.title',
     isGatsbyLink: false,
     url: 'https://diplomaterv.vik.bme.hu/hu/Browse.aspx?d=MIT',
-    count: 450
+    count: 498
+    /* From diplomaterv portal, 2010-2021 Summer */
   },
   {
     title: 'home.counters.c2.title',
     isGatsbyLink: false,
     url: 'http://tdk.bme.hu/Browse/Papers?f=VIK&d=MIT',
-    count: 109
+    count: 117
+    /* Until 2021-12: 38 OTDK (2001-) and 79 TDK (2009-) */
   },
   {
     title: 'home.counters.c3.title',
     isGatsbyLink: true,
     url: '/education#achievements',
-    count: 69
+    count: 80
+    /* Until 2021-12: UNKP (2016-2021): 18; Rektori: 4; PP thesis: 8; NFÖD: 25+; ACM-W: 1; CERN: 9+; Huawei: 7+; IBM: 4; Siemens: 2; OTDT: 2;
+       KBME: did not count... */
   }
 ]
 
