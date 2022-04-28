3e4f72e6d57b8fc7714f182150d32a941e97c211
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2022-04-28 08:58:56 +0000

    Sort former members based on locale

--- src/components/aboutpage-components/FormerMembersSection.tsx
+++ src/components/aboutpage-components/FormerMembersSection.tsx
@@ -11,20 +11,26 @@ type Props = {
 const FormerMembersSection: React.FC<Props> = ({ nodes }) => {
   const { t } = useTranslation()
 
+  function memberName(props: MemberProps) {
+    return t('about.members.name', { firstName: props.firstName, lastName: props.lastName })
+  }
+
   return (
     <Hero id="formermembers" bgImageUrl="/images/panorama.jpg" heroTitle="about.formermembers.heroTitle">
       <div className="card-columns alumni-list">
-        {nodes.map((formermember) => (
-          <div key={formermember.firstName + formermember.lastName} className="alumni">
-            {t('about.members.name', { firstName: formermember.firstName, lastName: formermember.lastName })}
-            {formermember.title ? `, ${formermember.title}` : ''}
-            {formermember.linkedInPage && (
-              <a target="_blank" rel="noopener noreferrer" href={formermember.linkedInPage}>
-                <FaLinkedin style={{ marginLeft: '0.25rem', marginBottom: '0.25rem' }} />
-              </a>
-            )}
-          </div>
-        ))}
+        {nodes
+          .sort((member1, member2) => memberName(member1).localeCompare(memberName(member2)))
+          .map((formermember) => (
+            <div key={formermember.firstName + formermember.lastName} className="alumni">
+              {memberName(formermember)}
+              {formermember.title ? `, ${formermember.title}` : ''}
+              {formermember.linkedInPage && (
+                <a target="_blank" rel="noopener noreferrer" href={formermember.linkedInPage}>
+                  <FaLinkedin style={{ marginLeft: '0.25rem', marginBottom: '0.25rem' }} />
+                </a>
+              )}
+            </div>
+          ))}
       </div>
     </Hero>
   )
