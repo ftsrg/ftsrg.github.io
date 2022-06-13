1d9bb00a21100ec3ad8d9ad1d9b6f2b0d6a27cd6
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2022-06-13 15:57:27 +0000

    Wrap application in SSRProvider as required by react-bootstrap

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,14 +1,15 @@
 import { Global } from '@emotion/react'
 import { GatsbyBrowser } from 'gatsby'
 import React from 'react'
+import { SSRProvider } from 'react-bootstrap'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
 import '~utils/scss/style.scss'
 import { globalStyles } from '~utils/styles'
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({ element }) => (
-  <>
+  <SSRProvider>
     <Global styles={globalStyles} />
     {element}
-  </>
+  </SSRProvider>
 )
