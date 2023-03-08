a502ca928ba3a21bfe378103e7bcae9075157f44
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2023-03-08 18:01:53 +0000

    Add subpage capabilities to breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -5,15 +5,25 @@ import { FaChevronRight } from 'react-icons/fa'
 
 type Props = {
   title: string
+  subpages?: { title: string; link: string }[]
 }
 
-const Breadcrumbs: React.FC<Props> = ({ title }) => {
+const Breadcrumbs: React.FC<Props> = ({ title, subpages }) => {
   const { t } = useI18next()
 
   return (
     <div className="custom-breadcrumbs py-3 border-bottom">
       <Container>
         <Link to="/">{t('nav.home.title')}</Link>
+        {subpages &&
+          subpages.map((subpage) => (
+            <React.Fragment key={subpage.link}>
+              <span className="mx-2">
+                <FaChevronRight size="0.6rem" />
+              </span>
+              <Link to={subpage.link}>{t(subpage.title)}</Link>
+            </React.Fragment>
+          ))}
         <span className="mx-2">
           <FaChevronRight size="0.6rem" />
         </span>
