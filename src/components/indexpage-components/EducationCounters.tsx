c572f00f21ea5880db696aace80c965d30cfca6c
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2025-03-26 14:18:40 +0000

    Update counter values

--- src/components/indexpage-components/EducationCounters.tsx
+++ src/components/indexpage-components/EducationCounters.tsx
@@ -9,21 +9,24 @@ const counterData = [
     title: 'home.counters.c1.title',
     isGatsbyLink: false,
     url: 'https://diplomaterv.vik.bme.hu/hu/Browse.aspx?d=MIT',
-    count: 498
+    count: 498,
+    suffix: '+'
     /* From diplomaterv portal, 2010-2021 Summer */
   },
   {
     title: 'home.counters.c2.title',
     isGatsbyLink: false,
     url: 'http://tdk.bme.hu/Browse/Papers?f=VIK&d=MIT',
-    count: 135
-    /* Until 2022-12: 38 OTDK (2001-) and 97 TDK (2009-) */
+    count: 230,
+    suffix: '+'
+    /* Until 2024-12: 38 OTDK (2001-) and 191 TDK (2000-) */
   },
   {
     title: 'home.counters.c3.title',
     isGatsbyLink: true,
     url: '/education#achievements',
-    count: 80
+    count: 80,
+    suffix: '+'
     /* Until 2021-12: UNKP (2016-2021): 18; Rektori: 4; PP thesis: 8; NFÖD: 25+; ACM-W: 1; CERN: 9+; Huawei: 7+; IBM: 4;
     Siemens: 2; OTDT: 2; KBME: did not count... */
   }
@@ -43,7 +46,11 @@ const EducationCounters: React.FC = () => {
           {counterData.map((data) => (
             <Col key={data.title} xs={12} md={4} className="text-center mb-4">
               <div className={`${showCounter ? 'counter-animated' : 'counter-unanimated'}`}>
-                <div>{showCounter && <CountUp end={data.count} duration={3} className="text-primary h1 font-weight-bold" />}</div>
+                <div>
+                  {showCounter && (
+                    <CountUp end={data.count} duration={3} suffix={data.suffix} className="text-primary h1 font-weight-bold" />
+                  )}
+                </div>
                 {data.isGatsbyLink ? (
                   <Link to={data.url}>{t(data.title)}</Link>
                 ) : (
