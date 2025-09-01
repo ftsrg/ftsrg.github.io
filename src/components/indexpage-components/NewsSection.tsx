d09df4ff600da843ac08251555fc05be1847fdd2
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2025-09-01 11:08:03 +0000

    Remove Twitter integration

--- src/components/indexpage-components/NewsSection.tsx
+++ src/components/indexpage-components/NewsSection.tsx
@@ -18,19 +18,13 @@ const NewsSection: React.FC = () => {
       <div id="news" className="news-updates" key={key}>
         <Helmet>
           <script defer crossOrigin="anonymous" src="https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v7.0" nonce="SRkXDokT" />
-          <script defer crossOrigin="anonymous" src="https://platform.twitter.com/widgets.js" />
         </Helmet>
         <Container>
           <h2 className="section-title-underline text-center mb-5">
             <span>{t('home.news.title')}</span>
           </h2>
           <Row className="justify-content-center text-center mb-5">
-            <Col lg={6} className="social-media-container mb-5 mb-lg-0">
-              <a className="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/ftsrg_bme?ref_src=twsrc%5Etfw">
-                {t('commons.ftsrgFullName')} on Twitter
-              </a>
-            </Col>
-            <Col lg={6} className="social-media-container">
+            <Col lg={12} className="social-media-container">
               <div
                 className="fb-page"
                 data-href="https://www.facebook.com/ftsrg/"
