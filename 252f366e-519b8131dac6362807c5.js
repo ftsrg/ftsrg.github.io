bfb11c9d671d17cab108cdd9d4ad8728094b93eb
micskeiz <micskeiz@users.noreply.github.com>
2026-05-13 16:12:04 +0000

    deployed via Gatsby Publish Action 🎩 for e4dd5caeda5a5f28af26f601d043d29dc5c5d9be

---
+++ 252f366e-519b8131dac6362807c5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkftsrg_next=self.webpackChunkftsrg_next||[]).push([[228],{5827:function(t,n,e){e.d(n,{eAf:function(){return r}});var a=e(5566);function r(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-519b8131dac6362807c5.js.map
